00bdceace59bbef052cdb4cc4eaa4eb749cb1b09
Dalinineseven <159402517+Dalinineseven@users.noreply.github.com>
2024-03-12 19:31:21 -0500

    Archivo de configuraciones globales
    Se establecen colores y tipos de letras para la interfaz

---
+++ config.py
@@ -0,0 +1,18 @@
+#COLORESS PANELES Y BOTONES
+COLOR_BARRA_SUPERIOR = "#fa5f49" #c63637 #fa5f49 #e2504c #6a9eda
+COLOR_BARRA_LATERAL = "#95b8f6" #5086c1 #6a9eda #95b8f6
+COLOR_CUERPO_PRINCIPAL = "#ffffff" #f9d99a #a0d2f3 #c3dff9
+COLOR_MENU_CURSOR_ENCIMA = "#add5fa"
+COLOR_CUERPO_TITULO = "#ffa500" #6a9eda #f9a59a #ffa500
+COLOR_CUERPO_SUBTITULO= "#96c4c4" #96c4c4 #a0d2f3
+COLOR_CUERPO_INFORMACION = "#c3dff9" #c3dff9 #bdbfbf
+COLOR_BOTON_ACT="#21AD64" #69C26F #42ab49 #21AD64
+COLOR_BOTON_DES="#bdbfbf"
+COLOR_SUBBOTON_DES= "#6a9eda" #6a9eda
+
+#TIPOS DE LETRAS
+TITULOS="Gill Sans MT" #Gill Sans MT #Grandview Display #Abadi #Harrington #Jokerman #HP Simplified #Kristen ITC #Showcard Gothic #Buxton Sketch #Franklin Gothic Heavy
+SUBTITULO="Tahoma" #Tahoma #Franklin Gothic Demi
+TEXTO="Univers Light" #Univers Light #Franklin Gothic Book
+BOTON_T="Franklin Gothic Medium" #Cascadia Code SemiLight #Franklin Gothic Medium
+TIEMPO = "Segoe UI Black"
